afceff06b187775805fc5706015b37030fe8c5ca
NAVEENAPRIYA J <jnaveenapriya2006@gmail.com>
2026-02-03 11:14:45 +0530

    Added User Authentication logic

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const db = require('./config/db');
-
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use('/api/auth', authRoutes);
 // Test Route
 app.get('/', (req, res) => {
     res.send('Backend Server is Running! 🚀');
